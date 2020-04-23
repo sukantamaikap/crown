f8f60a72438f2fffee9d3d0d2bd3fed74c8b415a
sukantamaikap <sukanta.maikap@gmail.com>
2020-04-23 12:01:07 +1000

    shop page added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import HomrePage from './pages/homepage/homepage.component';
 import HatsPage from './pages/hatpage/hatpage.component';
+import ShopPage from './pages/shop/shop.component';
 import { Route } from 'react-router-dom';
 function App() {
   return (
     <div>
       <switch>
         <Route exact path='/' component= {HomrePage} />
-        <Route path='/shop/hats' component = {HatsPage} />
+        <Route exact path='/shop' component = {ShopPage} />
+        <Route exact path='/shop/hats' component = {HatsPage} />
       </switch>
     </div>
   );
