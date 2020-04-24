9bcd2441848069903eaa2b36baccc77f3d827dce
sukantamaikap <sukanta.maikap@gmail.com>
2020-04-25 09:45:12 +1000

    adding header component along with the link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 import HomrePage from './pages/homepage/homepage.component';
 import HatsPage from './pages/hatpage/hatpage.component';
 import ShopPage from './pages/shop/shop.component';
-import { Route } from 'react-router-dom';
+import Header from './components/header/header-component';
+
 function App() {
   return (
     <div>
+      {/* Header will be part of all the pages. Anything that has to be part of all the pages
+      has to be be outside switch */}
+      <Header />
       <switch>
         <Route exact path='/' component= {HomrePage} />
         <Route exact path='/shop' component = {ShopPage} />
