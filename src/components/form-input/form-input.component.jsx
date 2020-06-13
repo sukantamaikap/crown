465267c2147b0b9a8259908f2f54ad5ee7c6bfec
sukantamaikap <sukanta.maikap@gmail.com>
2020-06-13 19:54:13 +1000

    fixing sing-in issue

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 
 import './form-input.styles.scss';
 
-const FormInput = ({handleChange, lable, ...otherProps}) => (
-    <div className="group">
-        <input className='form-input' onChang= {handleChange} {...otherProps} />
-        {
-            lable ? 
-            <lable className={`${otherProps.value.lenght} ? 'shrink' : ''} form-input-label`}> 
-                {lable}
-            </lable>
-            : null
-        }
-    </div>
-)
+const FormInput = ({ handleChange, label, ...otherProps }) => (
+  <div className='group'>
+    <input className='form-input' onChange={handleChange} {...otherProps} />
+    {label ? (
+      <label
+        className={`${
+          otherProps.value.length ? 'shrink' : ''
+        } form-input-label`}
+      >
+        {label}
+      </label>
+    ) : null}
+  </div>
+);
 
 export default FormInput;
