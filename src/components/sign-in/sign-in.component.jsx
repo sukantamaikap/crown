3025be2b44ae89b29849cfd43da4830827449d3c
sukantamaikap <sukanta.maikap@gmail.com>
2020-06-14 18:26:43 +1000

    minor fix for login with firebase

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -54,7 +54,7 @@ class SignIn extends React.Component {
           />
           <div className='buttons'>
             <CustomButton type='submit'> Sign in </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton onClick={signInWithGoogle}>
               Sign in with Google
             </CustomButton>
           </div>
