ef168e127142c3b317c8fd7529c3ab92accb00ca
sukantamaikap <sukanta.maikap@gmail.com>
2020-06-26 08:25:22 +1000

    adding cart icon in the homepage with svg bag icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 
-import {auth} from '../../firebase/firebase.utils'
+import {auth} from '../../firebase/firebase.utils';
+import { CartIcon } from '../cart-icon/cart-icon.component';
 
 import './header.styles.scss';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
@@ -25,6 +26,7 @@ const Header = ({currentUser}) => (
                 :
                 <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
     </div>
 );
