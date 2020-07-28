f42a79fc99e7f85b153a00f43df7b6adbf075aaa
sukantamaikap <sukanta.maikap@gmail.com>
2020-07-28 21:55:52 +1000

    refactored components

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
+import CollectionPage from '../collection/collection.component';
 import CollectionOverview  from '../../components/collections-overview/collections-overview.component';
 import './shop.styles.scss';
 
-const Shop = ({ collections }) => (
+const Shop = ({match}) => (
     <div className='shop-page'>
-        <CollectionOverview collections={collections}/>
+        <Route exact path={`${match.path}`} component={CollectionOverview}/>
+        {/* :categoryId lets you dynamically set the route. Whatever you typle after 
+        match.path will be assigned to :categoryId. To pass it on to the child access 
+        it as: match.params.categoryId */}
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
     </div>
 );
 
